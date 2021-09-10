3d6bc12a4dfa46b9c7db36f71c9426ecd8435700
Ramzy Syahrul <ramzy.sramadhan@gmail.com>
2021-09-10 13:23:14 +0700

    add create, update, getOne controller

--- src/controllers/productReview.js
+++ src/controllers/productReview.js
@@ -1,11 +1,59 @@
 const ProductReview = require("../models/productReview");
 class ProductReviewController {
   static async create(req, res) {
-    const { productId, userId, message, reviewImageURL, isAnonymous, rating } =
-      await req.body;
+    try {
+      const { productId, userId, message, reviewImageURL, isAnonymous, rating } = await req.body;
+
+      const createReview = await ProductReview.create({...req.body});
+
+      const resPayload = {
+        statusCode: 200,
+        statusText: "success",
+        message: "Create a review",
+        data: createReview
+      };
+
+      res.json(resPayload).status(200);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   }
-  static async getOne(req, res) {}
-  static async update(req, res) {}
+  static async getOne(req, res) {
+    try {
+      const id = req.params.id;
+      const getReview = await movie.findById(id)
+        .populate("productId")
+        .populate("userId");
+
+      const resPayload = {
+        statusCode: 200,
+        statusText: "success",
+        message: "Get a review",
+        data: getReview
+      };
+
+      res.json(resPayload).status(200);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+  static async update(req, res) {
+    try {
+      const id = req.params.id;
+      const updateReview = await ProductReview.findByIdAndUpdate(id, req.body);
+
+      const resPayload = {
+        statusCode: 200,
+        statusText: "success",
+        message: "Update a review",
+        data: updateReview
+      };
+
+      res.json(resPayload).status(200);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
   static async delete(req, res) {}
   static async getProductReviews(req, res) {}
   static async getAll(req, res) {}

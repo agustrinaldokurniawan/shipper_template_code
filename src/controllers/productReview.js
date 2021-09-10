63a4131fe0e2a42d5a4439473a5b02b126feed29
agustrinaldokurniawan <agustkurniawan010899@gmail.com>
2021-09-10 13:20:00 +0700

    [product review] delete, getProductReviews, getAll

--- src/controllers/productReview.js
+++ src/controllers/productReview.js
@@ -1,4 +1,5 @@
 const ProductReview = require("../models/productReview");
+
 class ProductReviewController {
   static async create(req, res) {
     const { productId, userId, message, reviewImageURL, isAnonymous, rating } =
@@ -6,7 +7,53 @@ class ProductReviewController {
   }
   static async getOne(req, res) {}
   static async update(req, res) {}
-  static async delete(req, res) {}
-  static async getProductReviews(req, res) {}
-  static async getAll(req, res) {}
+  static async delete(req, res) {
+    try {
+      const { userId, reviewId } = await req.body;
+
+      const responseDelete = await ProductReview.findOneAndDelete({
+        _id: reviewId,
+        userId: userId,
+      }).catch((err) => {
+        throw { message: "Your are not allow to delete this review" };
+      });
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+
+    return res.json(responseDelete);
+  }
+  static async getProductReviews(req, res) {
+    try {
+      const { productId } = await req.params;
+
+      const reviews = await ProductReview.aggregate([
+        { $group: { productId, averageRating: { $avg: "$rating" } } },
+      ])
+        .lean()
+        .catch((error) => {
+          throw { message: "Error while aggregate reviews", error };
+        });
+
+      return res.json(reviews);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+  static async getAll(req, res) {
+    try {
+      const reviews = await ProductReview.find({})
+        .lean()
+        .catch((error) => {
+          throw {
+            message: "Error while fetching reviews from database",
+            error: error,
+          };
+        });
+
+      return res.json(reviews);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
 }
